3afc1caaa9f1909bbd0c5d5644b42793e0ee17b5
kiranP <keyrun602@gmail.com>
2026-02-16 20:08:50 +0545

    feat: add Counter component using useState for increment, decrement and reset

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,18 +1,27 @@
 import ProductCard from "./components/ProductCard";
+import Counter from "./components/Counter";
+
+// export default function App() {
+//   const laptops = [
+//     { id: 1, name: "Dell", price: 800 },
+//     { id: 2, name: "HP", price: 750 },
+//     { id: 3, name: "Lenovo", price: 900 },
+//   ];
+
+//   return (
+//     <div>
+//       <h2>Products</h2>
+//       {laptops.map((lap) => (
+//         <ProductCard key={lap.id} name={lap.name} price={lap.price} />
+//       ))}
+//     </div>
+//   );
+// }
 
 export default function App() {
-  const laptops = [
-    { id: 1, name: "Dell", price: 800 },
-    { id: 2, name: "HP", price: 750 },
-    { id: 3, name: "Lenovo", price: 900 },
-  ];
-
   return (
     <div>
-      <h2>Products</h2>
-      {laptops.map((lap) => (
-        <ProductCard key={lap.id} name={lap.name} price={lap.price} />
-      ))}
+      <Counter />
     </div>
   );
 }
