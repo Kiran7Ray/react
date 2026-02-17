c0b07fe5d61392d62af1cb9bdcd343defc629d49
kiranP <keyrun602@gmail.com>
2026-02-17 17:38:14 +0545

    feat: create reusable Card component using children prop for flexible content.

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -31,31 +31,60 @@ import FocusInput from "./components/FocusInput";
 //   );
 // }
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navigation from "./components/Navigation";
-import Home from "./components/Home";
-import About from "./components/About";
-import UserProfile from "./components/UserProfile";
+// import Navigation from "./components/Navigation";
+// import Home from "./components/Home";
+// import About from "./components/About";
+// import UserProfile from "./components/UserProfile";
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <div style={{ padding: 20 }}>
+//         <h1>React Routing Example</h1>
+
+//         {/* Navigation links */}
+//         <Navigation />
+
+//         <hr />
+
+//         {/* Routes */}
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/user/:userId" element={<UserProfile />} />
+//         </Routes>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+import Card from "./components/Card";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div style={{ padding: 20 }}>
-        <h1>React Routing Example</h1>
+    <div style={{ padding: 20 }}>
+      <h1>Card Component Demo</h1>
 
-        {/* Navigation links */}
-        <Navigation />
+      {/* Card 1 */}
+      <Card>
+        <p>This is a paragraph inside card</p>
+      </Card>
 
-        <hr />
+      {/* Card 2 */}
+      <Card>
+        <button>Click Me</button>
+      </Card>
 
-        {/* Routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/user/:userId" element={<UserProfile />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+      {/* Card 3 */}
+      <Card>
+        <h3>Fruits</h3>
+        <ul>
+          <li>Apple</li>
+          <li>Banana</li>
+        </ul>
+      </Card>
+    </div>
   );
 }
--- react/src/components/Card.jsx
+++ react/src/components/Card.jsx
@@ -1,13 +1,13 @@
-// export default function Card(props) {
-//   return (
-//     <div
-//       style={{
-//         border: "2px solid black",
-//         padding: 15,
-//         margin: 10
-//       }}
-//     >
-//       {props.children}
-//     </div>
-//   );
-// }
+export default function Card(props) {
+  return (
+    <div
+      style={{
+        border: "2px solid black",
+        padding: 15,
+        margin: 10,
+      }}
+    >
+      {props.children}
+    </div>
+  );
+}
